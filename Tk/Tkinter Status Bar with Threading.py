87b7d5e81ca35a231ed19e37a30fe850d412e746
Kyle <kylesponable@gmail.com>
2021-11-01 16:34:46 -0400

    This python progrem demonstrates a tkinter status bar WITH threading

---
+++ Tk/Tkinter Status Bar with Threading.py
@@ -0,0 +1,46 @@
+#taken from here and language updated to english
+#https://stackoverflow.com/questions/63836979/bar-progress-in-tkinter-with-thread
+
+import threading
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Progress bar example")
+        self.button = tk.Button(self, text="Start", command=self.start_action)
+        self.button.pack(padx=10, pady=10)
+
+    def start_action(self):
+        self.button.config(state=tk.DISABLED)
+
+        t = threading.Thread(target=self.contar)
+        t.start()
+
+        self.windows_bar = WinProgressBar(self)
+
+    def contar(self):
+        for i in range(100000):
+            print("Is continuing", i)
+
+        print('stop')
+        self.windows_bar.progressbar.stop()
+        self.windows_bar.destroy()
+        self.button.config(state=tk.NORMAL)
+
+
+class WinProgressBar(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.title("Progress")
+        self.geometry("300x200")
+        self.progressbar = ttk.Progressbar(self, mode="indeterminate")
+        self.progressbar.place(x=30, y=60, width=200)
+        self.progressbar.start(20)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
