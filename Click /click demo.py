d272d716e21e6e0cab79bd3559b4e1517ea2b3b2
Kyle <kylesponable@gmail.com>
2021-11-21 20:24:58 -0500

    Added demo from multi threading

--- Click /click demo.py
+++ Click /click demo.py
@@ -8,7 +8,7 @@ import click
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
               help='The person to greet.')
-@click.option('--message',  help='What you wanna say') #For right now it works with no spacve Can click take whitespace?
+@click.option('--message',  help='What you wanna say') #For right now it works with no space Can click take whitespace?
 
 
 def hello(count, name, message):
@@ -18,6 +18,30 @@ def hello(count, name, message):
     c = click.get_current_context()
     message = c.params["message"]
     print("the message is " + message)
+'''
+Use above to save input to a variable for single threaded apps only there is another way for multi threads 
+    
+import click
+
+
+@click.pass_context
+def some_func(ctx, bar):
+    foo = ctx.params["foo"]
+    print(f"The value of foo is: {foo}")
+
+
+@click.command()
+@click.option("--foo")
+@click.option("--bar")
+def main(foo, bar):
+    some_func(bar)
+
+
+if __name__ == "__main__":
+    main()
+    
+Use this with multi thread 
+'''
 
 
 
