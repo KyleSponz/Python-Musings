a96069b7832b4cb3473daeda93d1367f58598546
Kyle <kylesponable@gmail.com>
2021-11-21 19:50:49 -0500

    Added taking input and storing to a variable

--- Click /click demo.py
+++ Click /click demo.py
@@ -1,5 +1,4 @@
 '''
-
 Taken from https://click.palletsprojects.com/en/8.0.x/
 '''
 
@@ -9,10 +8,19 @@ import click
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
               help='The person to greet.')
-def hello(count, name):
+@click.option('--message',  help='What you wanna say') #For right now it works with no spacve Can click take whitespace?
+
+
+def hello(count, name, message):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
         click.echo(f"Hello {name}!")
+    c = click.get_current_context()
+    message = c.params["message"]
+    print("the message is " + message)
+
+
+
 
 if __name__ == '__main__':
     hello()
